f542c5d041f67cebde87ebd625ba4347dfc7029e
Scott Weinstein <sweinstein27@gmail.com>
2018-05-01 18:32:26 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -34,12 +34,11 @@ array.inject { |sum, n| sum + n}
 end
 
 def add_s(array)
-array.each_with_index do { |word, index| 
+array.each_with_index do { |word, index|
   if index == 1
-    word 
-  else 
+    word
+  else
      word << "s"}
 end
 end
 end
-
